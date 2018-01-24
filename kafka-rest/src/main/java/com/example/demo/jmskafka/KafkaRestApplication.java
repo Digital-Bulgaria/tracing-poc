c548a53a51e83848689b407a38a8b965abb56bd0
Lachezar Balev <lachezar.balev@rewe-digital.com>
2018-01-24 14:44:23 +0200

    first commit

---
+++ kafka-rest/src/main/java/com/example/demo/jmskafka/KafkaRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.jmskafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRestApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(KafkaRestApplication.class, args);
+    }
+
+}
