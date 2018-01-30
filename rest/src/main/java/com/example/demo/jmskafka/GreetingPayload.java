66078b035d93093d3cbb970006933628a1ed2f61
Zahari Alexiev <zahari.aleksiev@rewe-digital.com>
2018-01-30 13:07:54 +0100

    implemented service 2 (kafka to rest) + fixing rest service 3

--- rest/src/main/java/com/example/demo/jmskafka/GreetingPayload.java
+++ rest/src/main/java/com/example/demo/jmskafka/GreetingPayload.java
@@ -1,6 +1,5 @@
 package com.example.demo.jmskafka;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -9,6 +8,7 @@ import java.time.LocalDate;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class GreetingPayload {
 
+  @JsonProperty("to")
   private String name;
 
   @JsonFormat(pattern = "yyyy-MM-dd")
@@ -16,10 +16,36 @@ public class GreetingPayload {
 
   private String message;
 
-  @JsonCreator
-  public GreetingPayload(@JsonProperty("name") String name,
-                         @JsonProperty("date") LocalDate date,
-                         @JsonProperty("message") String message) {
+  public String getName() {
+    return name;
+  }
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public LocalDate getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  @Override
+  public String toString() {
+    return "GreetingPayload{" +
+        "name='" + name + '\'' +
+        ", date=" + date +
+        ", message='" + message + '\'' +
+        '}';
   }
 }
