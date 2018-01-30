d97c5a9dba15cb24dc6110703e631f9a2dfa28f6
Lachezar Balev <lachezar.balev@gmail.com>
2018-01-30 10:24:44 +0200

    Just adding more skeletons

--- rest/src/main/java/com/example/demo/jmskafka/Service4Controller.java
+++ rest/src/main/java/com/example/demo/jmskafka/Service4Controller.java
@@ -2,6 +2,7 @@ package com.example.demo.jmskafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.sleuth.Span;
 import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +28,30 @@ public class Service4Controller {
     LOGGER.info("Service4");
     Thread.sleep(5000);
     return "Finish";
+  }
+
+  @GetMapping("/senkoExpenses")
+  public String calculateExpensesForSenko() {
+
+    LOGGER.info("Launching calculation for Senko's expenses...");
+
+    String expenses = "";
+
+    Span newSpan = this.tracer.createSpan("calculateExpenses");
+    try {
+      expenses = String.valueOf(600+200);
+      // ...
+      // You can tag a span
+      this.tracer.addTag("senkoExpenses", expenses);
+      // ...
+      // You can log an event on a span
+      newSpan.logEvent("senkoExpensesCalculated");
+    } finally {
+      // Once done remember to close the span. This will allow collecting
+      // the span to send it to Zipkin
+      this.tracer.close(newSpan);
+    }
+
+    return expenses;
   }
 }
