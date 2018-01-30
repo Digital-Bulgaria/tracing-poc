6fa29a5d5a7103dd15dc998de3b8775577830c75
georgi.peychev <georgi.peychev@rewe-digital.com>
2018-01-30 13:58:07 +0200

    added birthday service

---
+++ jms-kafka/src/main/java/com/example/demo/jmskafka/domain/Greeting.java
@@ -0,0 +1,46 @@
+package com.example.demo.jmskafka.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import java.time.LocalDate;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Greeting {
+  private String to;
+  private LocalDate date;
+  private String message;
+
+  public String getTo() {
+    return to;
+  }
+
+  public void setTo(String to) {
+    this.to = to;
+  }
+
+  public LocalDate getDate() {
+    return date;
+  }
+
+  public void setDate(LocalDate date) {
+    this.date = date;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  @Override
+  public String toString() {
+    return "Greeting{" +
+        "to='" + to + '\'' +
+        ", date=" + date +
+        ", message='" + message + '\'' +
+        '}';
+  }
+}
