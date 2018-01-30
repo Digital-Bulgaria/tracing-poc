a5280f8c52eb0e59fcf7fca8bdd372e77d4f7140
Svetoslav Kolev <svetoslav.kolev@rewe-digital.com>
2018-01-30 14:31:43 +0200

    enriching GreetingPayload

--- rest/src/main/java/com/example/demo/jmskafka/GreetingPayload.java
+++ rest/src/main/java/com/example/demo/jmskafka/GreetingPayload.java
@@ -11,15 +11,43 @@ public class GreetingPayload {
 
   private String name;
 
-  @JsonFormat(pattern = "yyyy-MM-dd")
+  @JsonFormat(pattern = "yyyy/MM/dd")
   private LocalDate date;
 
   private String message;
 
   @JsonCreator
-  public GreetingPayload(@JsonProperty("name") String name,
+  public GreetingPayload(@JsonProperty("to") String name,
                          @JsonProperty("date") LocalDate date,
                          @JsonProperty("message") String message) {
 
+    this.name = name;
+    this.date = date;
+    this.message = message;
+  }
+
+  public String getName() {
+
+    return name;
+  }
+
+  public LocalDate getDate() {
+
+    return date;
+  }
+
+  public String getMessage() {
+
+    return message;
+  }
+
+  @Override
+  public String toString() {
+
+    return "GreetingPayload{" +
+        "name='" + name + '\'' +
+        ", date=" + date +
+        ", message='" + message + '\'' +
+        '}';
   }
 }
